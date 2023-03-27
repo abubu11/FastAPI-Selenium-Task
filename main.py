cf0467d7a08b9329ff30b9ef40db6ec85473c6a2
Diego Eslava <d.eslava@uniandes.edu.co>
2023-03-26 20:15:08 -0500

    L

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
 
 
 
@@ -23,24 +25,27 @@ class Msg(BaseModel):
     msg: str
     secret: str
 
-urls='''https://datta07.github.io/classcentralclone/ 
-     https://heartfelt-lollipop-736861.netlify.app/ 
-     https://muhammederenaslan.github.io/codingallstars/
-     https://exosmotic-bay.000webhostapp.com/
-     https://www.facebook.com/'''
+
 expected_language="hi"
 resultado_lista=[]
 
 @app.post('/')
-def read_root():
-    url_list=urls.split("\n")
+def read_root(texto: str = Form(...)):
+
+    urls = texto
+
+    print(urls)
+
+    url_list=urls.split(" ")
+
+    print(url_list)
+
     i = -1
     for element in url_list:
         i+=1
-        print(element)
         resultado_lista.append({"url": element, "resultado": "", "WRONG_PAGE": "", "IMAGE_HIGH_RESOLUTION": "",
              "INNER_PAGES_NOT_TRANSLATED": "False", "JAVASCRIPT_DROPDOWN": ""})
-        PATH_TO_DRIVER = "/Users/diegofeslava/Documents/Documentos - MacBook de Abubu/PersonalProjects/FastAPI-Selenium-Task/chromedriver"
+        PATH_TO_DRIVER = os.environ.get("CHROMEDRIVER_PATH")
         driver= webdriver.Chrome(PATH_TO_DRIVER)
         driver.get(element)
 
@@ -56,7 +61,6 @@ def read_root():
 
             image_element = driver.find_element(By.XPATH, '/html/body/div[1]/main/section[1]/div[1]/img')
 
-            print(image_element.rect)
 
             try:
                 javascript_list = driver.find_element(By.XPATH, '/html/body/div[1]/header/div[1]/nav/div[1]/button[2]')
@@ -64,7 +68,7 @@ def read_root():
                 javascript_list_enabled = driver.find_element(By.XPATH,
                                                               '/html/body/div[1]/header/div[1]/nav/div[1]/nav/div[1]/section[1]/ul/li[2]/a/span[1]')
                 javascript_list_enabled.click()
-                resultado_lista[i] = {"url": element, "resultado": "", "IMAGE_BAD_RESOLUTION": "",
+                resultado_lista[i] = {"url": element, "resultado": "NOT_PASS", "IMAGE_BAD_RESOLUTION": "",
                                       "INNER_PAGES_NOT_TRANSLATED": "", "JAVASCRIPT_DROPDOWN_NOT_ENABLED": "True"}
                 pass
 
@@ -73,23 +77,24 @@ def read_root():
                                       "INNER_PAGES_NOT_TRANSLATED": "", "JAVASCRIPT_DROPDOWN_NOT_ENABLED": "False"}
                 pass
 
-            '''  inner_page=driver.find_element(By.XPATH,'/html/body/div[1]/main/section[1]/div[2]/ul/li[2]/a/span')
+            driver.get(element)
+
+            inner_page=driver.find_element(By.XPATH,'/html/body/div[1]/main/section[1]/div[2]/ul/li[2]/a/span')
             inner_page.click()
 
             time.sleep(1)
 
             html_element=driver.find_element(By.TAG_NAME,'html')
-            lang_attribute = html_element.get_attribute('lang')
+            lang_attribute = html_element.get_attribute('translate')
 
-            print(html_element)
-            print(lang_attribute)
 
-               driver.quit()
+
+            driver.quit()
 
             if lang_attribute == expected_language:
                 {"url": element, "resultado": "", "WRONG_PAGE": "", "IMAGE_HIGH_RESOLUTION": "",
                  "INNER_PAGES_NOT_TRANSLATED": "False", "JAVASCRIPT_DROPDOWN": ""}
-    '''
+
 
             pass
 
